a7469495984f9640630568137c1400adee6be19e
bluewow <dogseen@gmail.com>
2021-10-09 08:09:52 +0900

    I Am Rich

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/person.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
